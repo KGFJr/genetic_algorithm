803f664e89b1734108d736959aded845556cb856
KGFJr <112489262+KGFJr@users.noreply.github.com>
2022-08-30 13:04:09 -0300

    Update ga.py

--- genetic_alg/ga.py
+++ genetic_alg/ga.py
@@ -50,18 +50,18 @@ class GeneticAlg():
         if self.best_fitness is None:
             self.best_fitness=np.amax(self.fitness)
             self.best_solution=self.population[np.argmax(self.fitness)]
-            self.worst_fitness=np.amin(self.fitness)
-            self.worst_solution=self.population[np.argmin(self.fitness)]
+          #  self.worst_fitness=np.amin(self.fitness)
+           # self.worst_solution=self.population[np.argmin(self.fitness)]
         elif current_best>self.best_fitness:
             self.best_fitness=np.amax(self.fitness)
             self.best_solution=self.population[np.argmax(self.fitness)]
             self.fitness_hist.append((self.num_generations, current_best, 
                                   np.mean(self.fitness), np.std(self.fitness), current_worst))
-        elif current_worst>self.worst_fitness:
-            self.worst_fitness=np.amin(self.fitness)
-            self.worst_solution=self.population[np.argmin(self.fitness)]
-            self.fitness_hist.append((self.num_generations, current_best, 
-                                  np.mean(self.fitness), np.std(self.fitness), current_worst))
+       # elif current_worst>self.worst_fitness:
+        #    self.worst_fitness=np.amin(self.fitness)
+         #   self.worst_solution=self.population[np.argmin(self.fitness)]
+          #  self.fitness_hist.append((self.num_generations, current_best, 
+           #                       np.mean(self.fitness), np.std(self.fitness), current_worst))
         
 
     def generate_new_pop(self):
