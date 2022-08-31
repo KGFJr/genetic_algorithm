8d8053fd84ad71df0b7f1007379c00683cffe54d
KGFJr <112489262+KGFJr@users.noreply.github.com>
2022-08-31 14:20:03 -0300

    Update ga.py

--- genetic_alg/ga.py
+++ genetic_alg/ga.py
@@ -106,12 +106,12 @@ class GeneticAlg():
                   print("-"*79)
                   print('Generation', self.num_generations)
                   print("-"*79)
-                  print(f'Melhor Aptidão Atual:{self.fitness_hist[1]:.0f}')
-                  print(f'Aptidão Média:{self.fitness_hist[2]:.0f}')
-                  print(f'Desvio Padrão:{self.fitness_hist[3]:.0f}')
-                  print(f'Pior Aptidão Atual:{self.fitness_hist[4]:.0f}')
-                  print(f'Mediana:{self.fitness_hist[5]:.0f}')
-                  print('Melhor das Gerações: {:.0f}'.format(self.best_fitness))
+                 # print(f'Melhor Aptidão Atual:{self.fitness_hist[1]:.0f}')
+                 # print(f'Aptidão Média:{self.fitness_hist[2]:.0f}')
+                 # print(f'Desvio Padrão:{self.fitness_hist[3]:.0f}')
+                 # print(f'Pior Aptidão Atual:{self.fitness_hist[4]:.0f}')
+                 # print(f'Mediana:{self.fitness_hist[5]:.0f}')
+                 # print('Melhor das Gerações: {:.0f}'.format(self.best_fitness))
                                                                    
                   print('Elapsed Time:{}h:{}m:{}s, Estimated to completion:{}h,{}m, {}s'.format(int(h),int(m),int(s),
                                                 int(he),int(me),int(se)))
