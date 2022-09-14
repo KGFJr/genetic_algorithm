5fe08a220cbbfe61dd9bc869f80d5ce578c92d45
KGFJr <112489262+KGFJr@users.noreply.github.com>
2022-09-14 08:03:01 -0300

    Update crossovers.py

--- genetic_alg/crossovers.py
+++ genetic_alg/crossovers.py
@@ -5,11 +5,12 @@ def single_point_crossover(parents, crossover_p):
         b = 0.2
         l=len(parents[0])
         point=np.random.choice(l)
-        child1 = parents[0][:point]
-        child2 = parents[0][point:]
-        child1 =float(b)*parents[0][:point]+float(1-b)*parents[1][:point]
-        child2 =float(1-b)*parents[0][point:]+float(b)*parents[1][point:]
-        child=np.concatenate(child1,child2)
+       # child1 = parents[0][:point]
+       # child2 = parents[0][point:]
+       # child1 =float(b)*parents[0][:point]+float(1-b)*parents[1][:point]
+       # child2 =float(1-b)*parents[0][point:]+float(b)*parents[1][point:]
+       # child=np.concatenate(child1,child2)
+        child=np.concatenate((parents[0][:point],parents[1][point:]))
     else:
         child=parents[0]
     return child
