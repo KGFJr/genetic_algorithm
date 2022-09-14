507be341149cc22ee064b8a41c33ea34aab9d022
KGFJr <112489262+KGFJr@users.noreply.github.com>
2022-09-14 09:29:23 -0300

    Update crossovers.py

--- genetic_alg/crossovers.py
+++ genetic_alg/crossovers.py
@@ -5,13 +5,24 @@ def single_point_crossover(parents, crossover_p):
         b = 0.2
         l=len(parents[0])
         point=np.random.choice(l)
-        child1 = np.multiply(parents[0][:point],b) + np.multiply(parents[1][:point],1-b)
-        #float(b)*parents[0][:point]+float(1-b)*parents[1][:point]
-        child2 = np.multiply(parents[0][point:],1-b) + np.multiply(parents[1][point:],b)
-        #float(1-b)*parents[0][point:]+float(b)*parents[1][point:]
-        child = np.concatenate(child1,child2)
-        # child=np.concatenate((parents[0][:point],parents[1][point:]))
+        child11 = np.multiply(parents[0],b) 
+        child12 = np.multiply(parents[1],1-b)
+        
+        child21 = np.multiply(parents[0],1-b)
+        child22 = np.multiply(parents[1],b)
+        
+        child111 = child11 + child21
+        child222 = child12 + child12
+        
+        
+        #child11 = np.multiply(parents[0][:point],b) 
+        #child12 = np.multiply(parents[1][:point],1-b)
+        
+        #child21 = np.multiply(parents[0][point:],1-b)
+        #child22 = np.multiply(parents[1][point:],b)
+       
+        #child = np.concatenate(child1,child2)
+        child=np.concatenate((child111 [:point],child222[point:]))
     else:
         child=parents[0]
-    #print(parents[0])
     return child
